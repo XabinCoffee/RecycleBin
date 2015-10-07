d23a3fb84bd288b7c9d3b9db40cdd2ba5644da87
Xabin Rodriguez <xabin95@gmail.com>
2015-10-07 10:17:23 +0200

    lab02, 6 puntua egina

--- Erregistratu.php
+++ Erregistratu.php
@@ -1,27 +1,20 @@
 <?php  
-
-$izenabizen = $_POST["name"];  
-$email = $_POST["email"];  
-$pasahitza = $_POST["password"];  
-$tlf = $_POST["phone"];
-$esp = $_POST["espezialitatea"];
-$interesak = "Skydiving";
-
-//Interesak hartzerakoan arazo bat dela eta, hau gerorako...
-//<!--$interesak = $_POST["interests"];-->
-
-
 //konexioa egin datubasearekin
 $con = mysql_connect("mysql.hostinger.es","u966022868_xabin","xabino") or die($con);
-mysql_select_db("quiz",$con);
+mysql_select_db("u966022868_xabin",$con);
+
+//$con = mysql_connect("localhost","root","") or die($con);
+//mysql_select_db("quiz",$con);
 	
 //Erabiltzailea txertatu
-$sql = "INSERT INTO erabiltzaile (Izena, Pasahitza, Eposta, Telefonoa, Espezialitatea, Interesak) VALUES ('$_POST[name]','$_POST[password]','$_POST[email]','$_POST[phone]', '$_POST[espezialitatea]', '$_POST[espezialitatea]')";  
+$sql = "INSERT INTO erabiltzaile (Izena, Pasahitza, Eposta, Telefonoa, Espezialitatea, Interesak) VALUES ('$_POST[name]','$_POST[password]','$_POST[email]','$_POST[phone]', '$_POST[Espezialitatea]', '$_POST[interests]')";  
 if (!mysql_query($sql)){
 	die("Errorea: ".mysql_error());
 	}
 
 echo "1 line written";
+ 
+echo "<hr> <a href='IkusiErabiltzaileak2.php'>Ikusi Erabiltzaileak</a>";
 
 // Konexioa ixteko
 mysql_close ($con);
---
+++ IkusiErabiltzaileak2.php
@@ -0,0 +1,46 @@
+<?php
+
+//Konexioa sortu datubasearekin
+$con = mysql_connect("mysql.hostinger.es","u966022868_xabin","xabino") or die($con);
+mysql_select_db("u966022868_xabin",$con);
+
+//Probak lokalki egiteko
+//$con = mysql_connect("localhost","root","") or die($con);
+//mysql_select_db("quiz",$con);
+
+//Erabiltzaile taulatik dena aukeratu
+$query="SELECT * FROM erabiltzaile";
+$erabguztiak=mysql_query($query);
+
+$lerrokop=mysql_numrows($erabguztiak);mysql_close();
+
+echo "<b>
+<center>Erabiltzaileak</center>
+</b>
+<br>
+<br>";
+
+$i=0;
+
+
+//Jasotako erabiltzaile bakoitza inprimitzeko
+while ($i < $lerrokop){
+	$izenabizen=mysql_result($erabguztiak,$i,"Izena");
+	$email=mysql_result($erabguztiak,$i,"Eposta");
+	$pasahitza=mysql_result($erabguztiak,$i,"Pasahitza");
+	$tlf=mysql_result($erabguztiak,$i,"Telefonoa");
+	$esp=mysql_result($erabguztiak,$i,"Espezialitatea");
+	$interesak=mysql_result($erabguztiak,$i,"Interesak");
+
+	echo "<b>
+	$izenabizen $email</b>
+	<br>
+	$pasahitza<br>
+	$tlf<br>
+	$esp<br>
+	$interesak<hr>
+	<br>";
+	$i++;
+}
+
+?>
