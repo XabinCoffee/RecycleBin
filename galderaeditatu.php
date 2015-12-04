2186aa9634f37f4f50d6de1b485623ae2404e8eb
Xabin Rodriguez <xabin95@gmail.com>
2015-12-04 09:38:03 +0100

    ReviewQuizzes-eko AJAXa konpondu, eguneratu() funtzioa falta

--- galderaeditatu.php
+++ galderaeditatu.php
@@ -10,17 +10,25 @@ $i=0;
 
 foreach ($data as $row){
 	
+	if ($i==$id){
+	
 	echo" <div class='form-style'>
 
 <h1>Galdera editatu</h1>
 
-<input type='text' name='galdera' value='. $data->itemBody->p .' />
+<input type='text' id='galdera".$id."' name='galdera' value=". $row->itemBody->p ." />
 
-<input type='text' name='erantzuna' value='. $data->correctResponse->value .' />
+<input type='text' id='erantzuna".$id."' name='erantzuna' value=". $row->correctResponse->value ." />
 
-<input type='button' name='eguneratu' value='Ez diat ezerre iten jeje' />";
+<input type='text' id='konplexutasuna".$id"' name='konplexutasuna' value=". $row['konplexutasuna'] ." />
 
+<input type='text' id='gaia".$id."' name='gaia' value=". $row['subject'] ." />
+
+<input type='button' name='eguneratu' value='Eguneratu' onClick='eguneratu(". $id .")' />";
+
+	}
 	
+	$i++;
 }
 
 ?>
